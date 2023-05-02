e74d912dc6359ad2a5b2dce7989d0887f1d59761
Operu <sofus@dvinge.net>
2023-05-02 20:49:48 +0200

    Revert "State Machine start progress"
    This reverts commit 98f810dd00ce5d20d72cf7c3e4f3298a53934e0f.

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,27 +1,121 @@
 ﻿using System;
-using Player.States;
+using Player.Tools;
+using UI;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Utilities;
 
 namespace Player
 {
-    public class PlayerManager : MonoBehaviour
+    public enum PlayerState
     {
-        private PlayerBaseState currentState;
+        IDLE, // When nothing else is being done.
+        INTERACTING, // When interacting with objects, like clicking a button
+        PLACING, // When you are placing an object.
+        WIRING, // When you are dragging a wire
+        PAUSED // When you press "Esc" and pause the game
+    }
+    
+    public class PlayerManager : Singleton<PlayerManager>
+    {
         
-        private PlayerIdleState idleState = new PlayerIdleState();
-        private PlayerWiringState wiringState = new PlayerWiringState();
-        private PlayerPlacingState placingState = new PlayerPlacingState();
-        private PlayerPausedState pausedState = new PlayerPausedState();
-        
-        
-        private void Start()
+        public Vector3 MousePosition { get; private set; }
+        public PlayerState State { get; private set; } = PlayerState.IDLE;
+
+        [Header("Tools")]
+        public DestroyTool destroyTool;
+        public PlaceTool placeTool;
+        public SelectTool selectTool;
+        public WireTool wireTool;
+
+        public void UpdateMousePosition(Vector2 mousePos)
         {
-            currentState = idleState;
+            MousePosition = mousePos;
+            switch (State)
+            {
+                case PlayerState.IDLE:
+                    selectTool.UpdateSelection();
+                    break;
+                case PlayerState.INTERACTING:
+                    break;
+                case PlayerState.PLACING:
+                    placeTool.UpdatePlacingPosition();
+                    break;
+                case PlayerState.WIRING:
+                    wireTool.UpdateWirePreview();
+                    break;
+                case PlayerState.PAUSED:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
-        private void Update()
+        private void SetState(PlayerState newState)
         {
+            State = newState;
+        }
+
+        public void InteractInput(InputAction.CallbackContext context)
+        {
+            PlayerState newState = State;
             
+            if (State is PlayerState.PLACING && context.started)
+            {
+                placeTool.PlaceComponent();
+                newState = PlayerState.IDLE;
+            }
+
+            if (State is PlayerState.IDLE && context.started && selectTool.IsHoveringConnectable())
+            {
+                wireTool.StartWirePreview();
+                selectTool.CursorClicked();
+                newState = PlayerState.WIRING;
+            }
+
+            if (State is PlayerState.WIRING && context.canceled)
+            {
+                wireTool.StopWirePreview();
+                selectTool.DisableCursor();
+                selectTool.CursorClicked();
+                newState = PlayerState.IDLE;
+            }
+            
+            UpdateMousePosition(MousePosition);
+            SetState(newState);
+        }
+
+        public void RotateInput(InputAction.CallbackContext context)
+        {
+            if (State is PlayerState.PLACING && context.started)
+            {
+                placeTool.RotateComponent();
+            }
+        }
+
+        public void DestroyInput(InputAction.CallbackContext context)
+        {
+            if (State is PlayerState.IDLE && context.started)
+            {
+                destroyTool.TryDestroyObject();    
+            }
+        }
+
+        public void PauseInput(InputAction.CallbackContext context)
+        {
+            if (State is PlayerState.WIRING) wireTool.StopWirePreview();
+            if (State is PlayerState.PLACING) placeTool.PlaceComponent();
+            
+            SetState(PlayerState.PAUSED);
+        }
+
+        public void UIButtonInput(PlacementItem item)
+        {
+            if (State is PlayerState.IDLE)
+            {
+                SetState(PlayerState.PLACING);
+                placeTool.SelectComponent(item);
+            }
         }
     }
 }
--- Assets/Scripts/Player/States/PlayerBaseState.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Player.States
-{
-    public abstract class PlayerBaseState
-    {
-        public abstract void EnterState(PlayerManager player);
-        public abstract void UpdateState(PlayerManager player);
-    }
-}
